9849c1afdb05c4b0d2b1ca75b6084e7aba4a06fa
Cleiton Cotrin <cleitoncotrin@gmail.com>
2019-01-15 00:56:18 -0200

    update

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
+class Havainas extends Html
+{
+    public function __construct()
+    {
+        $this->template = 'havainas.html';
+    }
+}
+
 class Html
 {
-    public function build($message)
+    protected $template = 'default.html';
+
+    public function build($title, $message)
     {
-        $email_template_string = file_get_contents('template.html', true);
-        $email_template = printf($email_template_string, $message);
+        $email_template_string = file_get_contents($this->template, true);
+        $email_template = printf($email_template_string, $title, $message);
 
         return $email_template_string;
     }
@@ -22,6 +32,10 @@ $message = '
     </ul>
 ';
 
-$html->build($message);
+$title = 'Titulo';
+$html->build($title, $message);
+
+// $havainas = new Havainas();
+// $havainas->build($title, $message);
 
 
